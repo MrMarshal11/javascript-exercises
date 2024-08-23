2473c16332139bd2604f84aac3cf8ca6c46fb17d
Mr.Marshal <marshalrock11@gmail.com>
2024-08-23 16:08:31 +1000

    Completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    // Convert string inputs to numbers
+    position = parseInt(position);
 
+    // Handle edge cases
+    if (position < 0) return "OOPS";
+    if (position === 0) return 0;
+
+    // Create an array with the first two Fibonacci numbers
+    let array = [0, 1];
+
+    // Push numbers in a Fibonacci way until position is matched
+    for (let i = 2; i <= position; i++) {
+        array.push(array[i - 1] + array[i - 2]);
+    }
+
+    // Return number in the position which Fibonacci sequence is requested
+    return array[position];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
