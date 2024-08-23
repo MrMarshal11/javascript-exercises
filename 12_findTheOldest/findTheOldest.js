55330fe08ecf0084b5a969d05acbd6cded58e8cb
Mr.Marshal <marshalrock11@gmail.com>
2024-08-23 16:51:52 +1000

    Completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {  // Accept the 'people' array as a parameter
+    let oldest = people.reduce((oldestPerson, currentPerson) => {
+        const currentYear = new Date().getFullYear();
+        const currentAge = (currentPerson.yearOfDeath || currentYear) - currentPerson.yearOfBirth;
+        const oldestAge = (oldestPerson.yearOfDeath || currentYear) - oldestPerson.yearOfBirth;
 
+        return currentAge > oldestAge ? currentPerson : oldestPerson;
+    });
+
+    return oldest;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
